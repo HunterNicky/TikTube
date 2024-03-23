a8b4f884a7cc0f19d1774960e9c3f463591d3522
HunterNicky <nickyzaguine@gmail.com>
2024-03-22 21:01:33 -0300

    first commit

---
+++ backend/src/main/java/com/aps/tiktube/TiktubeApplication.java
@@ -0,0 +1,13 @@
+package com.aps.tiktube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktubeApplication.class, args);
+	}
+
+}
